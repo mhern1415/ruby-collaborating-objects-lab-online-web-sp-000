f9d3238f3b80f80b3587d2986b71ecba06f4d2de
Mark Hernandez <m.hern1415@gmail.com>
2019-05-19 20:51:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,11 @@
-class Song 
-  attr_accessor :name, :song 
-  
+class Song
+
+  attr_accessor :name, :artist
+
   def initialize(name)
-    @name = name 
+    @name = name
   end
-  
+
   def self.new_by_filename(file_name)
     song = file_name.split(" - ")[1]
     artist = file_name.split(" - ")[0]
@@ -12,12 +13,10 @@ class Song
     new_song.artist_name = artist
     new_song
   end
-  
+
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
   end
-  
-  
-end 
-  
+
+end
