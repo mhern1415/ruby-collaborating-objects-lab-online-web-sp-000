cf5f777eb89391a70ebdfd6f487d9d62a679965c
Mark Hernandez <m.hern1415@gmail.com>
2019-05-19 20:48:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,15 +13,10 @@ class Song
     new_song
   end
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
   
   
 end 
