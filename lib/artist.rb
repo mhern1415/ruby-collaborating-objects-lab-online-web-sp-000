4ee342bc4c3e4f1345f0ea58331da013f86476b9
Mark Hernandez <m.hern1415@gmail.com>
2019-05-19 20:31:02 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,42 @@
+class Artist 
+
+attr_accessor :name, :song
+
+@@all = [] 
+  
+def initialize(name)
+  @name = name 
+  @songs = [] 
+end
+  
+def add_song(song)
+  @songs << song
+end
+
+  
+def save
+ @@all << self
+end
+
+def print_songs
+  @songs.each do |song|
+  puts song.name
+  end
+end
+
+ def self.find_or_create_by_name(artist_name)
+    found_artist = self.all.find {|artist| artist.name == artist_name}
+    if found_artist
+      found_artist
+    else
+      new_artist = self.new(artist_name)
+      new_artist.save
+      new_artist
+    end
+  end
+    
+def self.all
+  @@all
+end 
+
+end
